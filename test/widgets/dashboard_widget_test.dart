983fb8a4d97caa41314179fe5c0bc82a95f73e3f
Jeff <jeffersoncerqueira75@gmail.com>
2022-11-21 23:50:18 -0300

    .

--- test/widgets/dashboard_widget_test.dart
+++ test/widgets/dashboard_widget_test.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:my_app/app_dependencies.dart';
 import 'package:my_app/database/dao/contacts_dao.dart';
 import 'package:my_app/src/features/home/home_page.dart';
 
+import '../flows/transferency_flow_test.mocks.dart';
 import '../matchers/matchers.dart';
-import 'dashboard_widget_test.mocks.dart';
 
 @GenerateMocks([ContactDAO])
 void main() {
   MockContactDAO mockContactDao = MockContactDAO();
+  MockTransactionWebClient mockTransactionWebClient =
+      MockTransactionWebClient();
   testWidgets("should show one image when the dashboard is loaded",
       (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-        home: HomePage(
-      contactDAO: mockContactDao,
-    )));
+    await tester.pumpWidget(AppDependencies(
+        contactDAO: mockContactDao,
+        transactionWebClient: mockTransactionWebClient,
+        child: const MaterialApp(home: HomePage())));
     final Finder image = find.byType(Image);
 
     expect(image, findsOneWidget);
@@ -23,9 +26,10 @@ void main() {
 
   testWidgets("should show the Transfer feature when de dashboard is loadaded",
       ((WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: HomePage(contactDAO: mockContactDao),
-    ));
+    await tester.pumpWidget(AppDependencies(
+        contactDAO: mockContactDao,
+        transactionWebClient: mockTransactionWebClient,
+        child: const MaterialApp(home: HomePage())));
 
     final transferFeatureItem = find.byWidgetPredicate(((widget) =>
         featureItemMatcher(widget, "Transfer", Icons.monetization_on)));
@@ -36,11 +40,10 @@ void main() {
   testWidgets(
       "should show the Transaction feature when the dashboard is loaded ",
       ((WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: HomePage(
+    await tester.pumpWidget(AppDependencies(
         contactDAO: mockContactDao,
-      ),
-    ));
+        transactionWebClient: mockTransactionWebClient,
+        child: const MaterialApp(home: HomePage())));
 
     final transactionFeatureItem = find.byWidgetPredicate((widget) =>
         featureItemMatcher(widget, "Transaction Feed", Icons.description));
